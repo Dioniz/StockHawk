bb2ba8ff10041d342fe4fb2a6cf393138d70e27e
Miguel Gil <miguelgilbr@gmail.com>
2017-05-30 11:51:21 +0100

    Update Widget Data with Content Provider Data

--- app/src/main/java/com/udacity/stockhawk/StockWidget.java
+++ app/src/main/java/com/udacity/stockhawk/StockWidget.java
@@ -3,26 +3,61 @@ package com.udacity.stockhawk;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.widget.RemoteViews;
+
+import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.sync.QuoteSyncJob;
 
 public class StockWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    private String[] data = {"-","00.00","0%"};
+    private Uri content_uri = Contract.BASE_URI.buildUpon().appendPath(Contract.PATH_QUOTE).build();
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, String[] data) {
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.symbol,data[0]);
+        views.setTextViewText(R.id.price, data[1]);
+        views.setTextViewText(R.id.change,data[2]);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    private void loadData(Context context) {
+        Cursor mCursor = context.getContentResolver().query(
+                content_uri,
+                Contract.Quote.QUOTE_COLUMNS.toArray(new String[Contract.Quote.QUOTE_COLUMNS.size()]),
+                null,
+                null,
+                null);
+
+        if (mCursor.moveToFirst()) {
+            do {
+                data[0] = mCursor.getString(Contract.Quote.POSITION_SYMBOL);
+                data[1] = mCursor.getString(Contract.Quote.POSITION_PRICE);
+                data[2] = mCursor.getString(Contract.Quote.POSITION_PERCENTAGE_CHANGE)+ " %";
+            } while (mCursor.moveToNext());
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (QuoteSyncJob.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            loadData(context);
+        }
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            loadData(context);
+            updateAppWidget(context, appWidgetManager, appWidgetId,data);
         }
     }
 
@@ -30,10 +65,8 @@ public class StockWidget extends AppWidgetProvider {
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
     }
-
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
 }
-
